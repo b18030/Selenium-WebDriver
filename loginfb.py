7f87578c3a92c230ff964021f98bad263e229050
TUSHAR GOYAL <43175862+tushargoyal22@users.noreply.github.com>
2020-01-16 15:53:14 +0530

    Update loginfb.py

--- loginfb.py
+++ loginfb.py
@@ -3,5 +3,5 @@ driver=webdriver.Chrome("/home/tushar/Documents/Selenium/chromedriver")
 driver.get("https://www.facebook.com")
 driver.find_element_by_name("email").send_keys("tushar22.tg.tg@gmail.com")
 
-driver.find_element_by_name("pass").send_keys("B18030goyal")
+driver.find_element_by_name("pass").send_keys("MaiChutiyanhihu")
 driver.find_element_by_id("loginbutton").click()
